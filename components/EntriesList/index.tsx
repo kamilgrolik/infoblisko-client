c2d17ffbe88d66a34791fe6919f9a4ff74aed335
Kamil Grolik <kamilgrolik@macbook-pro-kamil.home>
2020-09-24 02:43:51 +0200

    Pagination removal

--- components/EntriesList/index.tsx
+++ components/EntriesList/index.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
-import { useRouter } from 'next/router';
 import { Button, Row, Spinner } from 'reactstrap';
 import Entry from '../Entry';
 import { Query, QueryIncidentsArgs } from '../../common/types';
@@ -27,28 +26,13 @@ const INCIDENTS_QUERY = gql`
 `;
 
 const EntriesList = () => {
-  const pageQueryValue = useRouter().query.page;
-  const [entriesLimit, setEntriesLimit] = useState(10);
-  const [currentPage, setCurrentPage] = useState(1);
   const { loading, error, data } = useQuery<Query, QueryIncidentsArgs>(
     INCIDENTS_QUERY,
     {
-      variables: {
-        limit: entriesLimit,
-        start: currentPage > 1 ? (currentPage - 1) * entriesLimit : 0,
-      },
       // pollInterval: 5000 //TODO: scenario, when to use pollInterval
     },
   );
   const incidents = data?.incidents;
-  const incidentsTotalCount = data?.incidentsConnection?.aggregate?.totalCount;
-
-  useEffect(() => {
-    //check url query, and set current page
-    if (typeof pageQueryValue === 'string' && pageQueryValue !== '1') {
-      setCurrentPage(parseInt(pageQueryValue));
-    }
-  }, []);
 
   return (
     <>
@@ -70,16 +54,6 @@ const EntriesList = () => {
         {error && alert('Błąd wczytywania danych') && (
           <p>Błąd wczytywania danych</p>
         )}
-        {/*{incidentsTotalCount && incidentsTotalCount > entriesLimit && (*/}
-        {/*  <Row className='d-flex justify-content-center'>*/}
-        {/*    <Pagination*/}
-        {/*      entriesTotalCount={incidentsTotalCount}*/}
-        {/*      entriesLimit={entriesLimit}*/}
-        {/*      currentPage={currentPage}*/}
-        {/*      setCurrentPage={setCurrentPage}*/}
-        {/*    />*/}
-        {/*  </Row>*/}
-        {/*)}*/}
         <Row className='d-flex justify-content-center'>
           <Button color='primary'>Pokaż więcej wpisów</Button>
         </Row>
